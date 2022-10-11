f46283ad00e9a1f147738bef58657f9352ba7fb0
cogdotnet1685 <cogdotnet1685@CTSDOTNET803>
2022-10-11 09:12:42 +0530

    updated changes

--- LoanApplicationWCFService/DataContext/LoanEMIDataContext.cs
+++ LoanApplicationWCFService/DataContext/LoanEMIDataContext.cs
@@ -29,7 +29,7 @@ namespace LoanApplicationWCFService.DataContext
             cmd.Parameters.Add(EMIId);
             SqlParameter LoanId = new SqlParameter("@LoanId", loanEMI.LoanId);
             cmd.Parameters.Add(LoanId);
-            SqlParameter EMIAmout = new SqlParameter("@EMIAmout", loanEMI.EMIAmout);
+            SqlParameter EMIAmout = new SqlParameter("@EMIAmount", loanEMI.EMIAmout);
             cmd.Parameters.Add(EMIAmout);
             SqlParameter EMIPaymentDate = new SqlParameter("@EMIPaymentDate", loanEMI.EMIPaymentDate);
             cmd.Parameters.Add(EMIPaymentDate);
@@ -92,7 +92,7 @@ namespace LoanApplicationWCFService.DataContext
                 LoanEMIModel loanEMI = new LoanEMIModel();
                 loanEMI.EMIId = new Guid(dt.Rows[i]["EMIId"].ToString());
                 loanEMI.LoanId = new Guid(dt.Rows[i]["LoanId"].ToString());
-                loanEMI.EMIAmout = Convert.ToDecimal(dt.Rows[i]["EMIAmout"].ToString());
+                loanEMI.EMIAmout = Convert.ToDecimal(dt.Rows[i]["EMIAmount"].ToString());
                 loanEMI.EMIPaymentDate = dt.Rows[i]["EMIPaymentDate"].ToString();
                 loanEMI.EMINo = Convert.ToInt32(dt.Rows[i]["EMINo"].ToString());
                 loanEMIList.Add(loanEMI);
